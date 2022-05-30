3d7bdbbda5bfef7a5740b018fb2ba1625fe4eff1
dw-raymart <raymart@devwerkz.com>
2022-05-30 10:06:55 +0800

    Wild Willies | Desktop PDP Implementation
    Product Information/Details
    Product Description(2nd section)
    FAQ's

---
+++ assets/product.js
@@ -0,0 +1,32 @@
+class Product {
+  constructor() {
+    this.selector = {
+      mobileImgSelector: '[image-mobile-selector]',
+      desktopImgSelector: '[image-desktop-selector]'
+    }
+
+    this.init();
+  }
+  
+  renderDescriptionImage() {
+    const imageDesktop = document.querySelector(this.selector.desktopImgSelector);
+    const imageDesktopSrc = imageDesktop.getAttribute('src');
+
+    if (window.matchMedia("(max-width: 989px)").matches) {
+      const elem = document.createElement("img");
+      elem.setAttribute('src', imageDesktopSrc);
+      elem.setAttribute('class', 'img-fluid');
+      document.querySelector(this.selector.mobileImgSelector).appendChild(elem);
+
+      imageDesktop.remove();
+    }
+  }
+
+  init() {
+    if (document.querySelector(this.selector.desktopImgSelector)) {
+      this.renderDescriptionImage();
+    }
+  }
+}
+
+new Product();
---
+++ assets/tab.js
@@ -0,0 +1,41 @@
+class Tab {
+  constructor() {
+    this.classes = {
+      tabsLink: '.tabs-link',
+      tabContent: '.tab-content'
+    }
+
+    document.querySelectorAll(this.classes.tabsLink).forEach(element => {
+      element.addEventListener('click', this.selectTabActive.bind(this));
+    });;
+  }
+
+  selectTabActive(e) {
+    e.preventDefault();
+    const scroll = e.target.getAttribute('data-scroll'),
+      scroll_to = e.target.getAttribute('data-scroll-element');
+    // make tab active  
+    this.seTabActive(e.target);
+    if (scroll) {
+      console.log('scroll to', scroll_to);
+    } else {
+      this.removeActiveTabContent();
+      document.querySelector(`#content-${e.target.id}`).classList.add('active');
+    }
+  }
+
+  removeActiveTabContent() {
+    document.querySelectorAll(this.classes.tabContent).forEach(element => {
+      element.classList.remove('active');
+    });
+  }
+
+  seTabActive(target) {
+    document.querySelectorAll(this.classes.tabsLink).forEach(element => {
+      element.classList.remove('active');
+    });
+    target.classList.add('active');
+  }
+}
+
+new Tab();
