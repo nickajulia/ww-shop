19b99fd252a0ac0219e928f216016f41f7d69c18
dw-raymart <raymart@devwerkz.com>
2022-06-08 22:47:56 +0800

    Update global.js

--- assets/global.js
+++ assets/global.js
@@ -974,3 +974,15 @@ class StickyHeader extends HTMLElement {
   }
   }
   customElements.define('sticky-header', StickyHeader);
+
+var className = "onscroll";
+var scrollTrigger = 60;
+
+window.onscroll = function() {
+  // We add pageYOffset for compatibility with IE.
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByTagName("header")[0].classList.add(className);
+  } else {
+    document.getElementsByTagName("header")[0].classList.remove(className);
+  }
+};
