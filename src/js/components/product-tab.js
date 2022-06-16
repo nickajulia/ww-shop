3ac63f984adbc71666162e8f8f11d7298519a4ef
dw-raymart <>
2022-06-16 23:52:46 +0800

    Site optimization .2

---
+++ src/js/components/product-tab.js
@@ -0,0 +1,39 @@
+class ProductTab {
+  constructor() {
+    this.classes = {
+      tabsLink: '.tabs-link--scroll',
+      tabContent: '.tab-content'
+    }
+
+    document.querySelectorAll(this.classes.tabsLink).forEach(element => {
+      element.addEventListener('click', this.selectTabActive.bind(this));
+    });;
+  }
+
+  selectTabActive(e) {
+    e.preventDefault();
+    const scroll = e.target.getAttribute('data-scroll'),
+      scroll_to = e.target.getAttribute('data-scroll-element');
+    // make tab active  
+    this.seTabActive(e.target);
+    if (!scroll) {
+      this.removeActiveTabContent();
+      document.querySelector(`#content-${e.target.id}`).classList.add('active');
+    }
+  }
+
+  removeActiveTabContent() {
+    document.querySelectorAll(this.classes.tabContent).forEach(element => {
+      element.classList.remove('active');
+    });
+  }
+
+  seTabActive(target) {
+    document.querySelectorAll(this.classes.tabsLink).forEach(element => {
+      element.classList.remove('active');
+    });
+    target.classList.add('active');
+  }
+}
+
+export { ProductTab };
---
+++ src/js/templates/product.js
@@ -0,0 +1,46 @@
+import $ from "jquery";
+import { ProductTestimonial } from '../components/product-testimonial';
+
+$(document).ready(function() {
+
+  if(document.querySelector('[section-type="main-product"]')) {
+    $(".product-form__submit").click(function() {
+      setTimeout(function() {
+        $.getJSON('/cart.js', function(cart) {
+            //console.log(cart);
+            console.log("processing...");
+        }).done(function( cart ) {
+            console.log("done");
+            console.log(cart);
+
+            var free_shipping_price = parseFloat(window.ProductInfo.free_shipping_price);
+            var total_price = parseFloat(cart.total_price/100 );
+
+            console.log("free_shipping_price");
+            console.log(free_shipping_price);
+
+            console.log("total_price");
+            console.log(total_price);
+
+            if( parseFloat(total_price) >= parseFloat(free_shipping_price) ){
+              console.log(">=");
+              $('.shipping-bar-bg').css('background-color', '#f15523');
+              $('.shipping-tool').hide();
+              $('span.text').html("");
+              $('span.text').html("You've got free shipping");
+            }else{
+              console.log("<");
+              $('.shipping-bar-bg').removeAttr("style");
+              $('span.text').html("");
+              $('span.text').html(`Add <b>$ <span class='left-shipping-price'>${window.ProductInfo.free_shipping_left}</span></b> more to your cart for free shipping!`);
+            }
+            
+        });
+      }, 900);
+    });
+  }
+});
+
+if (document.querySelector('.product-testimonials')) {
+  new ProductTestimonial();
+}
